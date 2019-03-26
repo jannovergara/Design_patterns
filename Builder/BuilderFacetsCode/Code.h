c48b959f447ab4faf43b7a5fdcf3064f00ac43e9
Janno <Janno>
2019-03-26 17:42:35 +0800

    Builder facets

--- Builder/BuilderFacetsCode/Code.h
+++ Builder/BuilderFacetsCode/Code.h
@@ -1,3 +1,23 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <vector>
+
+struct Fields
+{
+    string type,
+    string name
+
+};
+
+class CodeBuilder;
+
+class Code
+{
+protected: // can be accessed in derived classes
+    string class_name;
+    vector<Fields> fields;
+public:
+    Code() {}
+    Code()
+}
--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Design patterns in modern C++
   - groovy builder
   - builder facet
 
-##### BuilderFacetsCode
+### BuilderFacetsCode
 
 You are asked to implement the Builder design pattern for rendering simple chunks of code.
 Sample use of the builder you are asked to create:
